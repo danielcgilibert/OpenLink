bd570fadecf967fe4294a75bfdc8f987bb8bfdbe
DanielCG <danielcgilibert@gmail.com>
2023-05-20 04:31:44 +0200

    🐛 fix(getUser.ts): handle errors and disconnect from prisma after query ✨ feat(getUser.ts): add prisma query to get user by email from session to return user object instead of session object

--- src/actions/getUser.ts
+++ src/actions/getUser.ts
@@ -1,5 +1,6 @@
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth/next'
+import prisma from '@/libs/prismadb'
 
 export async function getSession() {
   return getServerSession(authOptions)
@@ -8,8 +9,19 @@ export async function getSession() {
 export default async function getCurrentUser() {
   try {
     const session = await getSession()
-
     if (!session) return null
-    return session
-  } catch (error) {}
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user!.email as string,
+      },
+    })
+
+    if (!user) return null
+    return user
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await prisma.$disconnect()
+  }
 }

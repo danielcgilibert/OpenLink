adb9095bb22d78076faf29f0950d51e6f6be6705
DanielCG <danielcgilibert@gmail.com>
2023-05-22 20:26:34 +0200

    ✨ feat(Header.tsx): add Links and Appearance links to the header to improve navigation 🎨 style(Header.tsx): improve header style by adding a sticky position, a white background, and a rounded shape ✨ feat(UserDropdownMenu.tsx): add UserDropdownMenu component to display user information and sign out button 🎨 style(UserDropdownMenu.tsx): improve UserDropdownMenu style by adding a shadow and a rounded shape

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,30 @@
 import React from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+import UserDropdownMenu from './UserDropdownMenu'
 
 export default function Header({ currentUser }: any) {
   const router = useRouter()
 
   return (
-    <nav className="flex  justify-between border-b-2 border-gray-400 border-opacity-50 p-5">
+    <nav className=" rounded-full top-0  sticky w-full  left-0 h-16 shadow flex justify-between items-center  bg-white  p-5 ">
       <ul className="flex gap-5">
-        <li>Home</li>
-        <li>Settings</li>
-        <li>Users</li>
+        <Link
+          className="hover:bg-gray-100 px-2 py-1 rounded"
+          href={'/dashboard'}>
+          Links
+        </Link>
+        <Link
+          className="hover:bg-gray-100 px-2 py-1 rounded"
+          href={'/dashboard/appearance'}>
+          Aparrence
+        </Link>
       </ul>
 
       <div>
         {currentUser ? (
-          <img
-            onClick={() => signOut()}
-            src={currentUser.image}
-            alt="asdasd"
-            className="rounded-full w-9 h-9 cursor-pointer"
-          />
+          <UserDropdownMenu currentUser={currentUser} />
         ) : (
           <button
             onClick={() => signIn('google')}
---
+++ src/components/UserDropdownMenu.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { signOut } from 'next-auth/react'
+import { useState } from 'react'
+
+export default function UserDropdownMenu({ currentUser }: any) {
+  const [openMenu, setOpenMenu] = useState(false)
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setOpenMenu(!openMenu)}
+        onBlur={() => setOpenMenu(false)}
+        type="button"
+        className="flex  text-sm  rounded-full md:mr-0 focus:ring-4 focus:ring-gray-200 "
+        id="user-menu-button"
+        aria-expanded="false"
+        data-dropdown-toggle="user-dropdown"
+        data-dropdown-placement="bottom">
+        <span className="sr-only">Open user menu</span>
+        <img
+          referrerPolicy="no-referrer"
+          src={currentUser.image}
+          alt="user image"
+          className="rounded-full w-10  h-10  cursor-pointer"
+        />{' '}
+      </button>
+
+      {openMenu && (
+        <div
+          className="absolute z-10 visible my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
+          id="user-dropdown">
+          <div className="px-4 py-3">
+            <span className="block text-sm text-gray-900 ">
+              {currentUser.name}
+            </span>
+            <span className="block text-sm  text-gray-500 truncate ">
+              {currentUser.email}
+            </span>
+          </div>
+          <ul className="py-2 " aria-labelledby="user-menu-button">
+            <button
+              onClick={() => signOut()}
+              className=" px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 w-full  text-left">
+              Sign out
+            </button>
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}

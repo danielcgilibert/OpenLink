d50502ea96768e19c83378377a1e3426c66cb217
DanielCG <danielcgilibert@gmail.com>
2023-05-22 20:25:43 +0200

    🔥 chore(page.tsx): remove unused CreateBio and Dashboard components to declutter the codebase

--- src/app/create-bio/page.tsx
+++
@@ -1,9 +0,0 @@
-import FormBio from '@/components/FormBio'
-
-export default async function CreateBio() {
-  return (
-    <main className="flex flex-col min-h-screen   p-16">
-      <FormBio />
-    </main>
-  )
-}

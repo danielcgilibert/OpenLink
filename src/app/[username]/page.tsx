800a27234badeec22cbe5a9091e4137d74c4d5c2
DanielCG <danielcgilibert@gmail.com>
2023-05-22 20:26:50 +0200

    🔍 chore(layout.tsx): add metadata object to layout.tsx to improve SEO 🔍 chore(page.tsx): add getLinks function to fetch links data and display it in the page

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,4 +1,5 @@
 import getBio from '@/actions/getBio'
+import getLinks from '@/actions/getLinks'
 
 export default async function Page({
   params,
@@ -8,23 +9,30 @@ export default async function Page({
   const { username } = params
 
   const bio = await getBio(username)
-
-  console.log(username)
-
-  console.log(bio)
+  const links = await getLinks(bio?.id as number)
+  console.log(links)
 
   return (
-    <main className="bg-white text-black max-w-4xl mx-auto p-5">
-      <header>
+    <main className="flex flex-col   bg-slate-300  text-black max-w-4xl mx-auto p-5 h-full gap-12 md:px-44">
+      <header className="flex flex-col gap-2">
         <h1 className="text-3xl text-center">{bio?.name}</h1>
-
         <img
           className="rounded-full border-2 border-black  w-24 h-24 mx-auto mt-5 "
           src={bio?.avatar}
           alt="asdasd"
         />
+        <div>
+          <p className="text-center">{bio?.description}</p>
+        </div>
       </header>
-      hoLA HOA
+
+      <section className="flex flex-col gap-5">
+        {links?.map(link => (
+          <div className="bg-red-500 text-white p-5 w-full rounded">
+            {link.title}
+          </div>
+        ))}
+      </section>
     </main>
   )
 }

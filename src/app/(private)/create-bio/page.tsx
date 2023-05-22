9c4b5cc9f0f66f05f87a50164b5e0136da5c8239
DanielCG <danielcgilibert@gmail.com>
2023-05-22 20:25:30 +0200

    ✨ feat(create-bio): add CreateBio page component to render a FormBio component ✨ feat(layout): add UserLayout component to render a Header component and children components. Add metadata object to define title and description of the page. Add getCurrentUser function to get the current user.

---
+++ src/app/(private)/create-bio/page.tsx
@@ -0,0 +1,9 @@
+import FormBio from '@/components/FormBio'
+
+export default async function CreateBio() {
+  return (
+    <main className="flex flex-col min-h-screen   p-16">
+      <FormBio />
+    </main>
+  )
+}

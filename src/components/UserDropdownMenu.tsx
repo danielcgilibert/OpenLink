411f6f449fb8b156ff1173819fd15aff8d97d42e
DanielCG <danielcgilibert@gmail.com>
2023-05-24 15:52:12 +0200

    ♻️ refactor(UserDropdownMenu.tsx): fix import path for FadeIn component ✨ feat(UserDropdownMenu.tsx): add right-0 class to UserDropdownMenu component to align it to the right of the screen

--- src/components/UserDropdownMenu.tsx
+++ src/components/UserDropdownMenu.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { FadeIn } from '@/app/animations/FadeIn'
+import { FadeIn } from '@/animations/FadeIn'
 import { signOut } from 'next-auth/react'
 import { useState } from 'react'
 
@@ -20,15 +20,15 @@ export default function UserDropdownMenu({ currentUser }: any) {
           referrerPolicy="no-referrer"
           src={currentUser.image}
           alt="user image"
-          className="rounded-full w-10  h-10  cursor-pointer"
-        />{' '}
+          className="rounded-full w-10 h-10 cursor-pointer"
+        />
       </button>
 
       {openMenu && (
         <FadeIn
-          className="absolute z-10 visible my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
+          className="absolute z-50 right-0 visible my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
           id="user-dropdown">
-          <div className="px-4 py-3">
+          <div className="px-4 py-3 ">
             <span className="block text-sm text-gray-900 ">
               {currentUser.name}
             </span>
@@ -36,7 +36,7 @@ export default function UserDropdownMenu({ currentUser }: any) {
               {currentUser.email}
             </span>
           </div>
-          <ul className="py-2 " aria-labelledby="user-menu-button">
+          <ul className="py-2" aria-labelledby="user-menu-button">
             <button
               onClick={() => signOut()}
               className=" px-4 py-2 text-sm text-gray-700 hover:bg-gray-200 w-full  text-left">

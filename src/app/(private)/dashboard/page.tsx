154bef6cb7c6d7726f41b827ee4a3ce20a3889fb
DanielCG <danielcgilibert@gmail.com>
2023-05-24 15:53:37 +0200

    🎨 style(dashboard/page.tsx): refactor Dashboard page to use EditorLinks and MovilPreview components for better code organization and readability

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,9 +1,8 @@
 import getBio from '@/actions/getBio'
 import getLinks from '@/actions/getLinks'
-import FormAddLink from '@/components/FormAddLink'
-import LinkCard from '@/components/LinkCard'
-import Modal from '@/components/Modal'
 import { redirect } from 'next/navigation'
+import EditorLinks from '@/components/EditorLinks'
+import MovilPreview from '@/components/MovilPreview'
 
 export default async function Dashboard() {
   const bio = await getBio()
@@ -16,23 +15,9 @@ export default async function Dashboard() {
 
   return (
     <>
-      <div className="grid grid-cols-2 w-full ">
-        <section className="flex flex-col gap-5 p-5 ">
-          <FormAddLink />
-          <div className="flex flex-col gap-5">
-            {links?.map(link => (
-              <LinkCard key={link.id} link={link} />
-            ))}
-          </div>
-        </section>
-        <section>
-          <div className="  relative z-30 border-[10px] border-black rounded-[3rem]   w-[352px] h-[754px]    m-auto left-0 right-0 top-10">
-            <iframe
-              src={`/${bio?.username}`}
-              className="absolute  h-full  w-full  rounded-[3rem] "
-            />
-          </div>
-        </section>
+      <div className="grid md:grid-cols-2 w-full ">
+        <EditorLinks links={links} />
+        <MovilPreview username={bio.username} />
       </div>
     </>
   )

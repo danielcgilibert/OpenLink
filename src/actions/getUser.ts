a47a40674da4af8f80f5a61c68f91ac01451b520
DanielCG <danielcgilibert@gmail.com>
2023-05-24 15:54:14 +0200

    🐛 fix(getUser.ts): add type definition to session variable to avoid null reference error ✨ feat(getUser.ts): import Session type from next-auth to improve code readability and maintainability

--- src/actions/getUser.ts
+++ src/actions/getUser.ts
@@ -1,6 +1,7 @@
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth/next'
 import prisma from '@/libs/prismadb'
+import { Session } from 'next-auth'
 
 export async function getSession() {
   return getServerSession(authOptions)
@@ -8,7 +9,8 @@ export async function getSession() {
 
 export default async function getCurrentUser() {
   try {
-    const session = await getSession()
+    const session: Session | null = await getSession()
+
     if (!session) return null
 
     const user = await prisma.user.findUnique({

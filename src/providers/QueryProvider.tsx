cc9d61ffbe71e4054ac845febcc7c5b63bebe76c
DanielCG <danielcgilibert@gmail.com>
2023-05-28 23:14:12 +0200

    ✨ feat(QueryProvider.tsx): add QueryProvider component to provide a QueryClient instance to the app and set default options for queries.

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export default function QueryProvider({ children }: React.PropsWithChildren) {
+  const [client] = React.useState(
+    new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } })
+  )
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
+  )
+}

06b81dab984d38704862484f9476e410cd69d6b1
DanielCG <danielcgilibert@gmail.com>
2023-05-28 23:15:43 +0200

    🐛 fix(EditorLinks.tsx): fix ListLinks component not updating when new link is added by passing setListLinks function to FormAddLink component 💄 style(EditorLinks.tsx): remove unused useEffect import to improve code readability

--- src/components/EditorLinks.tsx
+++ src/components/EditorLinks.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import FormAddLink from './FormAddLink'
 import ListLinks from './ListLinks'
 import { Link } from '@prisma/client'
@@ -11,17 +11,18 @@ export default function EditorLinks({
   links: Link[] | null | undefined
 }) {
   const [isEditing, setIsEditing] = useState(false)
+  const [listLinks, setListLinks] = useState(links)
 
   return (
     <section className="flex flex-col gap-5 p-5 ">
-      <FormAddLink setIsEditing={setIsEditing} />
+      <FormAddLink setIsEditing={setIsEditing} setListLinks={setListLinks} />
 
       <div
         className={clsx(
           isEditing && 'blur-[2px] pointer-events-none',
           'transition-all ease-in delay-75 duration-200'
         )}>
-        <ListLinks links={links} />
+        <ListLinks links={listLinks} />
       </div>
     </section>
   )

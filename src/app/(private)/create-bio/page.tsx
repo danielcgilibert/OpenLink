fd9e616ba9097aaad1ac87fff3b5b82788103455
DanielCG <danielcgilibert@gmail.com>
2023-05-30 23:42:20 +0200

    🐛 fix(create-bio): redirect to dashboard if bio already exists ✨ feat(create-bio): add background image and improve layout of create bio page

--- src/app/(private)/create-bio/page.tsx
+++ src/app/(private)/create-bio/page.tsx
@@ -1,9 +1,33 @@
+import getBio from '@/actions/getBio'
 import FormBio from '@/components/FormBio'
+import { redirect } from 'next/navigation'
 
 export default async function CreateBio() {
+  const bio = await getBio()
+
+  if (bio) {
+    redirect('/dashboard')
+  }
   return (
-    <main className="flex flex-col min-h-screen   p-16">
-      <FormBio />
-    </main>
+    <>
+      <div>
+        <img
+          className="absolute -z-10 w-screen h-screen inset-0 object-cover"
+          src="Cloudy.svg"
+          alt="background"
+        />
+      </div>
+      <div className="relative  h-screen">
+        <main className="absolute left-0 right-0 top-1/4 w-full flex flex-col gap-5   bg-white  p-36  shadow-2xl rounded-lg">
+          <h1 className="text-black text-[32px] font-semibold leading-heading lg:text-5xl">
+            Create your BIO
+          </h1>
+          <h2 className="text-gray-500 text-[32px] font-semibold leading-heading  lg:text-xl">
+            Choose your OpenLink username. You can always change it later.
+          </h2>
+          <FormBio />
+        </main>
+      </div>
+    </>
   )
 }

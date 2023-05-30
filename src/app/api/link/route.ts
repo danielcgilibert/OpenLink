56adaeb8a8d434fb55460354c031a7f2639c282a
DanielCG <danielcgilibert@gmail.com>
2023-05-30 04:54:55 +0200

    🚀 feat(route.ts): add PUT method to update link data 🐛 fix(route.ts): fix getCurrentUser import statement 🐛 fix(route.ts): fix linter errors and warnings 🔥 chore(route.ts): remove console.log statement

--- src/app/api/link/route.ts
+++ src/app/api/link/route.ts
@@ -2,6 +2,36 @@ import getCurrentUser from '@/actions/getUser'
 import { NextResponse } from 'next/server'
 import prisma from '@/libs/prismadb'
 import { validUrl } from '@/libs/validUrl'
+export async function GET(request: Request) {
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser) {
+    return NextResponse.error()
+  }
+
+  const bio = await prisma.bio.findUnique({
+    where: {
+      userId: currentUser.id as string,
+    },
+  })
+
+  if (!bio) {
+    return NextResponse.error()
+  }
+
+  const links = await prisma.link.findMany({
+    where: {
+      bioId: bio?.id as number,
+    },
+  })
+
+  await prisma.$disconnect()
+
+  return NextResponse.json({
+    ok: true,
+    data: links,
+  })
+}
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser()
@@ -45,5 +75,50 @@ export async function POST(request: Request) {
   return NextResponse.json({
     ok: true,
     link: newLink,
+  })
+}
+
+export async function PUT(request: Request) {
+  const currentUser = await getCurrentUser()
+
+  if (!currentUser) {
+    return NextResponse.error()
+  }
+
+  const { id, input, type } = await request.json()
+
+  if (!id || !input || !type) {
+    return NextResponse.error()
+  }
+
+  let data = {}
+  if (type === 'url') {
+    data = {
+      url: input,
+    }
+  } else {
+    data = {
+      title: input,
+    }
+  }
+
+  console.log(data)
+
+  const link = await prisma.link.update({
+    where: {
+      id: id,
+    },
+    data,
+  })
+
+  if (!link) {
+    return NextResponse.error()
+  }
+
+  await prisma.$disconnect()
+
+  return NextResponse.json({
+    ok: true,
+    data: link,
   })
 }

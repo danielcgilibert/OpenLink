2ec1ba365eeea435a3e1028055891b5f03087d4d
DanielCG <danielcgilibert@gmail.com>
2023-05-30 23:45:02 +0200

    🎨 style(LinkCard.tsx): add delete button to LinkCard component 🐛 fix(LinkCard.tsx): fix LinkCard layout to properly display delete button and inputs

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -1,22 +1,38 @@
 'use client'
 import { Link } from '@prisma/client'
-import { DragIcon } from './Icons'
+import { DragIcon, TrashIcon } from './Icons'
 import EditorInput from './EditorInput'
 
 export default function LinkCard({ link }: { link: Link }) {
+  const handleDelete = async () => {
+    fetch(`/api/link/${link.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  }
   return (
-    <div className="flex  gap-5 rounded-lg py-8 p-5 bg-white shadow">
+    <div className="flex   gap-5 rounded-lg py-9 p-5 bg-white shadow">
       <div className="flex justify-center items-center">
         <DragIcon className="w-5 h-5" />
       </div>
-      <div className="flex flex-col">
-        <EditorInput
-          id={link.id}
-          text={link.title}
-          type="text"
-          className="font-bold"
-        />
-        <EditorInput id={link.id} text={link.url} type="url" />
+      <div className="flex flex-col  w-full">
+        <div className="flex flex-col gap-1">
+          <EditorInput
+            id={link.id}
+            text={link.title}
+            type="text"
+            className="font-bold"
+          />
+          <EditorInput id={link.id} text={link.url} type="url" />
+        </div>
+
+        <div className="flex justify-end items-end ">
+          <button className="p-1" onClick={handleDelete}>
+            <TrashIcon className="w-5 h-5" />
+          </button>
+        </div>
       </div>
     </div>
   )

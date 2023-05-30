dc38957c72c9da1b39f7699c2be8c244da2f9056
DanielCG <danielcgilibert@gmail.com>
2023-05-30 04:54:29 +0200

    🐛 fix(ListLinks.tsx): add class name to animated div to fix width issue 🎨 style(ListLinks.tsx): increase delay time for transitions to improve user experience

--- src/components/ListLinks.tsx
+++ src/components/ListLinks.tsx
@@ -6,12 +6,12 @@ export default function ListLinks({ links }: any) {
   const transitions = useTransition(links, {
     from: { opacity: 0 },
     enter: { opacity: 1 },
-    delay: 25,
+    delay: 150,
   })
   return (
     <div className="flex flex-col gap-5">
       {transitions((style, link) => (
-        <animated.div style={style}>
+        <animated.div style={style} className="min-w-full">
           <LinkCard key={link.id} link={link} />
         </animated.div>
       ))}

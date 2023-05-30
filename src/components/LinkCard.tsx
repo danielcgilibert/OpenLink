6d435135ac21f0ca983cbd05b9c588afb357fcb3
DanielCG <danielcgilibert@gmail.com>
2023-05-30 04:54:15 +0200

    🎨 style(LinkCard.tsx): refactor LinkCard component to use EditorInput component for better code organization and reusability

--- src/components/LinkCard.tsx
+++ src/components/LinkCard.tsx
@@ -1,26 +1,22 @@
+'use client'
 import { Link } from '@prisma/client'
-import { DragIcon, PencilIcon } from './Icons'
+import { DragIcon } from './Icons'
+import EditorInput from './EditorInput'
 
 export default function LinkCard({ link }: { link: Link }) {
   return (
-    <div className="flex  gap-5   rounded-lg py-8 p-5 bg-white shadow  ">
+    <div className="flex  gap-5 rounded-lg py-8 p-5 bg-white shadow">
       <div className="flex justify-center items-center">
         <DragIcon className="w-5 h-5" />
       </div>
-      <div className="flex flex-col gap-1">
-        <div className="flex items-center gap-1">
-          <span>{link.title}</span>
-          <span>
-            <PencilIcon />
-          </span>
-        </div>
-        <div className="flex items-center gap-1">
-          <a href={link.url}>{link.url}</a>
-
-          <span className="cursor-pointer">
-            <PencilIcon />
-          </span>
-        </div>
+      <div className="flex flex-col">
+        <EditorInput
+          id={link.id}
+          text={link.title}
+          type="text"
+          className="font-bold"
+        />
+        <EditorInput id={link.id} text={link.url} type="url" />
       </div>
     </div>
   )

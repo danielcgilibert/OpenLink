15f9296235dbc676fcc8a85892849d9300b2eb7e
DanielCG <danielcgilibert@gmail.com>
2023-05-30 04:54:02 +0200

    🐛 fix(MovilPreview.tsx): add Loading component to show loading state while fetching links data ✨ feat(MovilPreview.tsx): add links section to show links data from API and refactor code to use destructured variables

--- src/components/MovilPreview.tsx
+++ src/components/MovilPreview.tsx
@@ -1,35 +1,63 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query'
+import { Link } from '@prisma/client'
+import Loading from './Loading'
 
 export default function MovilPreview({ username }: { username: string }) {
-  const { data, isLoading } = useQuery({
-    queryKey: ['todos'],
+  const { data: bioData } = useQuery({
+    queryKey: ['bio'],
     queryFn: () => {
       return fetch(`/api/bio/${username}`).then(res => res.json())
     },
   })
 
-  if (isLoading) return <div>Loading...</div>
+  const { data: links, isLoading } = useQuery({
+    queryKey: ['links'],
+    refetchInterval: 500,
+    queryFn: () => {
+      return fetch(`/api/link`).then(res => res.json())
+    },
+  })
+
   return (
     <section>
-      <div className="hidden md:block relative -z-10 border-[10px] border-black rounded-[3rem] w-[352px] h-[754px] m-auto left-0 right-0 top-10">
-        <header className="flex flex-col justify-center items-center gap-3   p-5 border-b-2  border-zinc-800">
-          <img
-            className="rounded-full border-[4px] border-black  w-24 h-24   "
-            src={data.bio.avatar}
-            alt="avatar"
-          />
-
-          <div>
-            <h1 className="text-3xl">{data.bio.name}</h1>
-            <p className="hidden md:text-sm">{data.bio.description}</p>
+      <div className="hidden  md:block relative -z-10 border-[10px] border-black rounded-[3rem] overflow-auto w-[352px] h-[754px] m-auto left-0 right-0 top-10 ">
+        {isLoading ? (
+          <div className="flex justify-center items-center min-h-full">
+            <Loading />
           </div>
-        </header>
+        ) : (
+          <>
+            <header className="flex flex-col justify-center items-center gap-3   p-5 border-b-2  border-zinc-800">
+              <img
+                className="rounded-full border-[4px] border-black  w-24 h-24   "
+                src={bioData.bio.avatar}
+                alt="avatar"
+              />
+
+              <div>
+                <h1 className="text-3xl">{bioData.bio.name}</h1>
+                <p className="hidden md:text-sm">{bioData.bio.description}</p>
+              </div>
+            </header>
+            <section className="flex flex-col gap-5   text-center p-5  ">
+              {links.data?.map((link: Link) => (
+                <div
+                  key={link.id}
+                  className="bg-zinc-800 text-white p-5    rounded ">
+                  {link.title}
+                </div>
+              ))}
+            </section>
+          </>
+        )}
+
         {/* <iframe
-          src={`/${username}`}
-          className="absolute  h-full  w-full  rounded-[3rem] "
-        /> */}
+            src={`/${username}`}
+            className="absolute  h-full  w-full  rounded-[3rem] "
+          /> */}
+        {/* */}
       </div>
     </section>
   )

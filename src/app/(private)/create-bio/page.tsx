5a1eb5d17ccf1abf4f173d41cd90eb5698dff378
DanielCG <danielcgilibert@gmail.com>
2023-06-02 09:31:22 +0200

    🎨 style(create-bio/page.tsx): improve layout and design of create bio page 🔥 chore(layout.tsx): remove unused imports and code

--- src/app/(private)/create-bio/page.tsx
+++ src/app/(private)/create-bio/page.tsx
@@ -8,26 +8,29 @@ export default async function CreateBio() {
   if (bio) {
     redirect('/dashboard')
   }
+
   return (
     <>
-      <div>
-        <img
-          className='absolute inset-0 -z-10 h-screen w-screen object-cover'
-          src='Cloudy.svg'
-          alt='background'
-        />
-      </div>
-      <div className='relative  h-screen'>
-        <main className='absolute left-0 right-0 top-1/4 flex w-full flex-col gap-5   rounded-lg  bg-white  p-36 shadow-2xl'>
-          <h1 className='leading-heading text-[32px] font-semibold text-black lg:text-5xl'>
-            Create your BIO
-          </h1>
-          <h2 className='leading-heading text-[32px] font-semibold text-gray-500  lg:text-xl'>
-            Choose your OpenLink username. You can always change it later.
-          </h2>
+      <div className='flex h-full items-center justify-center'>
+        <div className='flex h-full flex-col    gap-5 rounded-xl bg-white  p-8   pt-24 shadow-2xl md:h-auto md:p-24'>
+          <header>
+            <h1 className='leading-heading text-4xl font-semibold text-primary lg:text-5xl'>
+              Create your BIO
+            </h1>
+            <h2 className='leading-heading mt-2  text-secondary'>
+              Choose your OpenLink username. You can always change it later.
+            </h2>
+          </header>
+
           <FormBio />
-        </main>
+
+          <span className='text-center text-sm text-light'>
+            Already have an account?{' '}
+            <span className='text-violet-500 underline'>Log in</span>
+          </span>
+        </div>
       </div>
+      <div className='gradient'></div>
     </>
   )
 }

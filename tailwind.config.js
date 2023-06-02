b5511d50f981c8d5745900260f01d508fd5f1432
DanielCG <danielcgilibert@gmail.com>
2023-06-02 09:31:02 +0200

    🎨 style(tailwind.config.js): update colors object to include primary, secondary and light colors to improve readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      colors: {
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        light: 'var(--light)',
       }
     }
   },

6c9201c8850c63543e06e65aeb29f980687547ff
DanielCG <danielcgilibert@gmail.com>
2023-06-05 08:47:10 +0200

    🎨 style(page.tsx): improve page layout and design ✨ feat(page.tsx): add generateMetadata function to generate dynamic page metadata based on the username parameter 🐛 fix(page.tsx): fix ExternalLink component import statement to use correct path

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,7 +1,16 @@
 import { getLinks } from '@/server/services/getLinks'
 import { getPublicBio } from '@/server/services/getPublicBio'
+import { ExternalLink } from '@/ui/Link'
 import { Link } from '@prisma/client'
 import { redirect } from 'next/navigation'
+
+export async function generateMetadata({
+  params
+}: {
+  params: { username: string }
+}) {
+  return { title: params.username }
+}
 
 export default async function Page({
   params
@@ -17,29 +26,37 @@ export default async function Page({
   }
   const links = await getLinks(bio?.id as number)
   return (
-    <main className='mx-auto flex   h-screen max-w-4xl flex-col  gap-12 bg-neutral-100 p-5 text-black  shadow-xl md:px-44'>
-      <header className='flex flex-col items-center justify-center gap-3   border-b-2 border-zinc-800  p-5'>
-        <img
-          className='h-24 w-24 rounded-full  border-[4px] border-black   '
-          src={bio?.avatar}
-          alt='avatar'
-        />
+    <>
+      <main className=' mx-auto flex h-screen   max-w-4xl flex-col gap-6  p-5  text-black    md:px-44'>
+        <header className='flex flex-col items-center justify-center gap-3   border-b-2 border-zinc-800  p-5'>
+          <img
+            className='h-24 w-24 rounded-full  border-[2px]'
+            src={bio?.avatar}
+            alt='avatar'
+          />
 
-        <div>
-          <h1 className='text-3xl'>{bio?.name}</h1>
-          <p className='hidden md:text-sm'>{bio?.description}</p>
-        </div>
-      </header>
-
-      <section className='flex flex-col gap-5 text-center'>
-        {links?.map((link: Link) => (
-          <div
-            key={link.id}
-            className='w-full rounded bg-zinc-800 p-5 text-white '>
-            {link.title}
+          <div>
+            <h1 className='text-2xl'>{bio?.name}</h1>
+            <p className='hidden md:text-sm'>{bio?.description}</p>
           </div>
-        ))}
-      </section>
-    </main>
+        </header>
+
+        <section className='flex flex-col gap-5 text-center'>
+          <section className='flex flex-col gap-5 text-center '>
+            {links?.map((link: Link) => (
+              <ExternalLink
+                key={link.id}
+                href={link.url}
+                className='w-full  rounded-full bg-violet-700 p-3  text-white  transition-colors first-letter:uppercase hover:bg-violet-800 '>
+                {link.title}
+              </ExternalLink>
+            ))}
+          </section>
+        </section>
+      </main>
+
+      <div className='backdrop-blur-5xl absolute left-0 right-0 top-0  -z-10 h-screen w-full  '></div>
+      <div className='gradient'></div>
+    </>
   )
 }

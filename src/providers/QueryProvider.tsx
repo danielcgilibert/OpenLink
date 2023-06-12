f016168c18a0bbee3a13603cfaa36bb861ba27a6
DanielCG <danielcgilibert@gmail.com>
2023-06-12 07:06:59 +0200

    🐛 fix(MovilPreview.tsx): add useEffect hook to set loading state when iframe is loaded 🎨 style(MovilPreview.tsx): change border radius to 4rem to match design 🔥 chore(QueryProvider.tsx): remove commented out ReactQueryDevtools component

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -2,17 +2,11 @@
 
 import React from 'react'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export default function QueryProvider({ children }: React.PropsWithChildren) {
   const [client] = React.useState(
     new QueryClient({ defaultOptions: { queries: { staleTime: 5000 } } })
   )
 
-  return (
-    <QueryClientProvider client={client}>
-      {children}
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
-  )
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
 }

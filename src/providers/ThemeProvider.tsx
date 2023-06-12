e95deed910a256d3910d4877502c9043b2b0dce9
DanielCG <danielcgilibert@gmail.com>
2023-06-12 07:06:46 +0200

    🎨 style(ThemeProvider.tsx): format code and add missing imports ✨ feat(ThemeProvider.tsx): add ThemeContext to provide theme state and setTheme function to children components 🆕 feat(updateTheme.ts): add updateTheme function to update user's theme in the database

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,17 +1,43 @@
 'use client'
+import { createContext, useEffect, useRef, useState } from 'react'
+import { themes as themesList } from '@/styles/themes'
+import { useIsFetching } from '@tanstack/react-query'
 
-import { ThemeProvider as Provider } from 'next-themes'
+export const ThemeContext = createContext({
+  loading: true,
+  setLoading: (loading: boolean) => {},
+  setTheme: (theme: string) => {}
+})
 
-export default function ThemeProvider({ children }: React.PropsWithChildren) {
+export default function ThProvider({ children }: React.PropsWithChildren) {
+  const [theme, setTheme] = useState('')
+  const [loading, setLoading] = useState(true)
+  const first = useRef(false)
+  const isFetching = useIsFetching()
+  useEffect(() => {
+    if (!first.current) {
+      first.current = true
+      return
+    }
+
+    if (loading) {
+      return
+    }
+
+    const iFrame = document.getElementById(
+      'preview-iframe'
+    ) as HTMLIFrameElement
+
+    if (iFrame) {
+      //@ts-ignore
+      iFrame.contentWindow.document.querySelector('#userBio').className =
+        themesList[theme]
+    }
+  }, [theme])
+
   return (
-    <Provider
-      themes={['neon', 'light', 'dark']}
-      value={{ neon: 'neon', dark: 'dark', light: 'light' }}
-      attribute='data-theme'
-      enableColorScheme={false}
-      defaultTheme='light'
-      enableSystem={false}>
+    <ThemeContext.Provider value={{ loading, setLoading, setTheme }}>
       {children}
-    </Provider>
+    </ThemeContext.Provider>
   )
 }
